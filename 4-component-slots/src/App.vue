3823b0e42aba9361ec70a472ecc93f78a1072bb8
Erdi Sener <erdi.sener@sisu9.com>
2022-01-05 22:53:31 +0300

    componenet slots

---
+++ 4-component-slots/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<!--   <Modal :OnSave="onsave" :title="'Erdi'" :content="'neler oluyor bizeneler oluyor'" /> -->
+
+  <Modal>
+    <template #title>
+      <h3>Slot ile gelen title bilgisi</h3>
+      </template>
+    <template #content>
+      <p>Slot ile gelen içerik bilgisi</p>
+      </template>
+  </Modal>
+</template>
+
+<script>
+import Modal from '@/components/Modal'
+
+export default {
+  components: {
+    Modal,
+  },
+  methods: {
+    onsave() {
+      alert("Saved");
+    }
+  }
+}
+</script>
---
+++ 4-component-slots/src/components/Modal.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <div class="header">
+            <slot name="title"/>
+        </div>
+        <div class="content">
+            <slot name="content"/>
+        </div>
+        <div class="footer text-right">
+            <button>Kapat</button>
+            <button @click="OnSave" class="green">Kaydet</button>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props : ["title","content","OnSave"]
+}
+</script>
